5f01153280090f92c69b0b402db4fbb7eddd512f
mht08 <47961256+mht08@users.noreply.github.com>
2019-05-03 15:16:25 +0800

    commit

---
+++ src/main/java/cn/train/entity/User.java
@@ -0,0 +1,108 @@
+package cn.train.entity;
+
+import java.util.Date;
+
+/*
+ * @ Copyright (c) Create by JASON  Date:2018-04-14  All rights reserved.
+ *
+ * @ class description：用户信息实体
+ *
+ */
+public class User {
+	
+	private String username;
+	private String password;
+	private Long id;
+	private String realname;
+	private String address;
+	private String telphone;
+	private Date birthday;
+	private String createUser;
+	private String createTime;
+	private String updateUser;
+	private String updateTime;
+	private String version;
+	private String delFalg;
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getRealname() {
+		return realname;
+	}
+	public void setRealname(String realname) {
+		this.realname = realname;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getTelphone() {
+		return telphone;
+	}
+	public void setTelphone(String telphone) {
+		this.telphone = telphone;
+	}
+	public Date getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+	public String getCreateUser() {
+		return createUser;
+	}
+	public void setCreateUser(String createUser) {
+		this.createUser = createUser;
+	}
+	public String getCreateTime() {
+		return createTime;
+	}
+	public void setCreateTime(String createTime) {
+		this.createTime = createTime;
+	}
+	public String getUpdateUser() {
+		return updateUser;
+	}
+	public void setUpdateUser(String updateUser) {
+		this.updateUser = updateUser;
+	}
+	public String getUpdateTime() {
+		return updateTime;
+	}
+	public void setUpdateTime(String updateTime) {
+		this.updateTime = updateTime;
+	}
+	public String getVersion() {
+		return version;
+	}
+	public void setVersion(String version) {
+		this.version = version;
+	}
+	public String getDelFalg() {
+		return delFalg;
+	}
+	public void setDelFalg(String delFalg) {
+		this.delFalg = delFalg;
+	}
+
+}
+
+
